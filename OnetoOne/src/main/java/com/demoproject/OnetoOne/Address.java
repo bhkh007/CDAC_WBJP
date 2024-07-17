701daac3139528d378be5370b153fdf4bcb0c515
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-17 13:10:44 +0530

    Address table model in One to One database relation

---
+++ OnetoOne/src/main/java/com/demoproject/OnetoOne/Address.java
@@ -0,0 +1,42 @@
+package com.demoproject.OnetoOne;
+
+public class Address {
+
+	
+	private int id;
+	private String street;
+	private String area;
+	private String city;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street=" + street + ", area=" + area + ", city=" + city + "]";
+	}
+	
+}
